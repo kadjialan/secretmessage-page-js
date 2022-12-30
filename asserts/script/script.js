756ad1bb86278f2732a1e33a417729d5256ef48b
kadjialan <alankadji7@gmail.com>
2022-12-30 15:03:09 +0100

    html modification

--- asserts/script/script.js
+++ asserts/script/script.js
@@ -5,21 +5,21 @@ const decodeButton = document.querySelector('.button')
 const input = document.querySelector('.text1')
 
 decodeButton.onclick = function encode() {
-let text = (input.value).toLowerCase()
-let empty = text.replace(/[^\w]/g, '')
-decode1.innerHTML = `${empty}`
-let col = Math.ceil(Math.sqrt(empty.length))
-console.log(col) 
-let chunk = ''
-let message = ''
-for (let i = 0; i< col; i++) {
-for (let j = i; j < empty.length; j+=col) {
-message += empty[j]
-chunk += empty[j]
-}
-console.log (`${chunk += '\n'}`)
-}
-console.log(`${chunk}`)
-decode2.innerHTML = chunk
-decode3.innerHTML = message
+    let text = (input.value).toLowerCase()
+    let empty = text.replace(/[^\w]/g, '')
+    decode1.innerHTML = `${empty}`
+    let col = Math.ceil(Math.sqrt(empty.length))
+    console.log(col)
+    let chunk = ''
+    let message = ''
+    for (let i = 0; i < col; i++) {
+        for (let j = i; j < empty.length; j += col) {
+            message += empty[j]
+            chunk += empty[j]
+        }
+        console.log(`${chunk += '\n'}`)
+    }
+    console.log(`${chunk}`)
+    decode2.innerHTML = chunk
+    decode3.innerHTML = message
 } 
