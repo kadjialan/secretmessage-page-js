b7b09d9112865f461279db3bf6f4bf3bb9d95743
kadjialan <alankadji7@gmail.com>
2022-12-30 17:20:49 +0100

    js modification

--- asserts/script/script.js
+++ asserts/script/script.js
@@ -4,22 +4,26 @@ const decode3 = document.getElementById('decoder3')
 const decodeButton = document.querySelector('.button')
 const input = document.querySelector('.text1')
 
-decodeButton.onclick = function encode() {
-    let text = (input.value).toLowerCase()
-    let empty = text.replace(/[^\w]/g, '')
-    decode1.innerHTML = `${empty}`
-    let col = Math.ceil(Math.sqrt(empty.length))
-    console.log(col)
-    let chunk = ''
-    let message = ''
-    for (let i = 0; i < col; i++) {
-        for (let j = i; j < empty.length; j += col) {
-            message += empty[j]
-            chunk += empty[j]
-        }
-        console.log(`${chunk += '\n'}`)
+decodeButton.onclick = function encode () {
+  const text = (input.value).toLowerCase()
+  console.log(text.length)
+  if (text.length>=50) {
+  const empty = text.replace(/[^\w]/g, '')
+  decode1.innerHTML = `${empty}`
+  const col = Math.ceil(Math.sqrt(empty.length))
+  let chunk = ''
+  let message = ''
+  for (let i = 0; i < col; i++) {
+    for (let j = i; j < empty.length; j += col) {
+      message += empty[j]
+      chunk += empty[j]
     }
-    console.log(`${chunk}`)
-    decode2.innerHTML = chunk
-    decode3.innerHTML = message
-} 
+    console.log(`${chunk += '\n'}`)
+  }
+  decode2.innerHTML = chunk
+  decode3.innerHTML = message
+}else {
+  alert('input a string of atleast 50 characters')
+}
+}
+
